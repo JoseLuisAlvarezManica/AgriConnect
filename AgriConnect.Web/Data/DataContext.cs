d8574974bd7ebd9e9e4838af05075539f5e86c84
JoseLuisAlvarezManica <114442646+JoseLuisAlvarezManica@users.noreply.github.com>
2025-02-10 12:42:40 -0600

    Controlador de ciudades con sus vistas respectivas.

---
+++ AgriConnect.Web/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using AgriConnect.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AgriConnect.Web.Data
+{
+    public class DataContext:DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+           
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>().HasIndex(x => x.Name).IsUnique();
+        }
+        
+
+    }
+}
